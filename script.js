ad3bc5df6fd2b85f6fe25214aec8b307c04eec11
kunal kumar <105341082+kunalkasera456@users.noreply.github.com>
2023-07-10 13:10:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
+	let firstName = document.getElemetByName(fname)[0].value;
+	let lastName = document.getElemetByName(lname)[0].value;
+
+	alert(firstName +" "+ lastName);
 
 }
